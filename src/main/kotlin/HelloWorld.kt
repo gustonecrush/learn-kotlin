305e8bcd563445629ec5a58b304ccbaea7dcdae3
gustonecrush <codeboxezz@gmail.com>
2022-08-07 06:13:16 +0700

    hello world in kotlin

---
+++ src/main/kotlin/HelloWorld.kt
@@ -0,0 +1,10 @@
+fun main() {
+
+    /*
+    * println => print something with adding new line
+    * print => print something without adding new line
+    * */
+
+    println("Hello World!")
+    println("Hello World!")
+}
