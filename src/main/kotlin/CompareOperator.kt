2adaf27ca1b29a93ac8e0998886ebf016f7a47e2
gustonecrush <codeboxezz@gmail.com>
2022-08-07 09:57:48 +0700

    compare operator in kotlin

---
+++ src/main/kotlin/CompareOperator.kt
@@ -0,0 +1,15 @@
+fun main() {
+
+    /*
+    * Comparison Operators
+    * >, <, >=, <=, ==, !=
+    * */
+
+    val a = 100
+    val b = 200
+
+    val result: Boolean = a > b
+
+    println(result)
+
+}
