c73a51fa3c97b7ec5d43657c89e1830e8ba82069
gustonecrush <codeboxezz@gmail.com>
2022-08-07 10:15:59 +0700

    for loops in kotlin

---
+++ src/main/kotlin/ForLoops.kt
@@ -0,0 +1,16 @@
+fun main() {
+
+    val heroes = arrayOf("Captain America", "Black Widow", "Punisher")
+    for (hero in heroes) {
+        println(hero)
+    }
+
+    for (value in 0..100) {
+        println(value)
+    }
+
+    for (value in 10 downTo 0 step 2) {
+        println(value)
+    }
+
+}
