8970850cfecdc0ecfaf1692f9c8a59b7d9a4bfc4
gustonecrush <codeboxezz@gmail.com>
2022-08-07 10:03:49 +0700

    if expression (if, else, else if) in kotlin

---
+++ src/main/kotlin/IfExpression.kt
@@ -0,0 +1,13 @@
+fun main() {
+
+    val examValue = 70
+
+    if(examValue > 85) {
+        println("Good Job!")
+    } else if(examValue > 60) {
+        println("Not Bad!")
+    } else {
+        println("Try again next year!")
+    }
+
+}
